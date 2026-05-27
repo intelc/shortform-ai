d17b4701e21fbc39a552d7fe7f7f52b6507dc766
Intel <intel@intelchen.com>
2026-05-27 11:46:38 -0400

    Finalize Homebrew formula

--- Formula/shortform-ai.rb
+++ Formula/shortform-ai.rb
@@ -3,46 +3,57 @@ class ShortformAi < Formula
 
   desc "Local-first CLI for short-form video and creator analysis"
   homepage "https://github.com/intelc/shortform-ai"
-  url "https://github.com/intelc/shortform-ai/archive/refs/tags/v0.1.12.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  url "https://files.pythonhosted.org/packages/93/ef/52690b3810bf21b1e64f74d0297820d85ba22e9a3f601f7a23b7fd57f1a6/shortform_ai-0.1.12.tar.gz"
+  sha256 "8c190df8f9a44782ca80ea13a30cb508aca134f36806ef00c6ee3c74b96c5838"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "rust" => :build
   depends_on "ffmpeg"
+  depends_on "python@3.12"
 
   resource "cryptography" do
-    url "https://files.pythonhosted.org/packages/source/c/cryptography/cryptography-45.0.7.tar.gz"
-    sha256 "REPLACE_WITH_CRYPTOGRAPHY_SHA256"
+    url "https://files.pythonhosted.org/packages/9f/a9/db8f313fdcd85d767d4973515e1db101f9c71f95fced83233de224673757/cryptography-48.0.0.tar.gz"
+    sha256 "5c3932f4436d1cccb036cb0eaef46e6e2db91035166f1ad6505c3c9d5a635920"
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/source/c/certifi/certifi-2025.8.3.tar.gz"
-    sha256 "REPLACE_WITH_CERTIFI_SHA256"
+    url "https://files.pythonhosted.org/packages/f3/ce/ee2ecad540810a79593028e88299baeae54d346cc7a0d94b6199988b89b1/certifi-2026.5.20.tar.gz"
+    sha256 "69dea482ab64caa7b9f6aba1c6bf48bb6a5448d1c0f1b17ab42ad8c763a5344d"
+  end
+
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/eb/56/b1ba7935a17738ae8453301356628e8147c79dbb825bcbc73dc7401f9846/cffi-2.0.0.tar.gz"
+    sha256 "44d1b5909021139fe36001ae048dbdde8214afa20200eda0f64c068cac5d5529"
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/source/c/charset_normalizer/charset_normalizer-3.4.3.tar.gz"
-    sha256 "REPLACE_WITH_CHARSET_NORMALIZER_SHA256"
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/source/i/idna/idna-3.10.tar.gz"
-    sha256 "REPLACE_WITH_IDNA_SHA256"
+    url "https://files.pythonhosted.org/packages/1a/88/bcf9709822fe69d02c2a6a77956c98ce6ea8ca8767a9aadcedc7eb6a2390/idna-3.16.tar.gz"
+    sha256 "d7a6da03db833450fca25d2358ac9ff06cd624577a4aea3a596d5c0f77b8e03d"
+  end
+
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/1b/7d/92392ff7815c21062bea51aa7b87d45576f649f16458d78b7cf94b9ab2e6/pycparser-3.0.tar.gz"
+    sha256 "600f49d217304a5902ac3c37e1281c9fe94e4d0489de643a9504c5cdfdfc6b29"
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.32.5.tar.gz"
-    sha256 "REPLACE_WITH_REQUESTS_SHA256"
+    url "https://files.pythonhosted.org/packages/ac/c3/e2a2b89f2d3e2179abd6d00ebd70bff6273f37fb3e0cc209f48b39d00cbf/requests-2.34.2.tar.gz"
+    sha256 "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/source/u/urllib3/urllib3-2.5.0.tar.gz"
-    sha256 "REPLACE_WITH_URLLIB3_SHA256"
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
   end
 
   resource "yt-dlp" do
-    url "https://files.pythonhosted.org/packages/source/y/yt-dlp/yt_dlp-2025.8.22.tar.gz"
-    sha256 "REPLACE_WITH_YT_DLP_SHA256"
+    url "https://files.pythonhosted.org/packages/8b/34/7c6b4e3f89cb6416d2cd7ab6dab141a1df97ab0fb22d15816db2c92148c9/yt_dlp-2026.3.17.tar.gz"
+    sha256 "ba7aa31d533f1ffccfe70e421596d7ca8ff0bf1398dc6bb658b7d9dec057d2c9"
   end
 
   def install
